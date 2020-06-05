6e5549b18e7aae63ccaa4e434d1632b576f5b42c
Blagoje Ivanovic <blagoje.r.ivanovic@gmail.com>
2020-06-05 14:23:04 +0200

    Add quiet argument to abcdepth

--- R/abcdepth.R
+++ R/abcdepth.R
@@ -2,9 +2,10 @@
 #'
 #' @param data A matrix (or dataframe) with the data within which to calculate depth
 #' @param point The point for which to calculate the depth (make sure that ncol(data) == length(point))
+#' @param quiet Suppress java debug messages
 #' @return The depth of the point within the given dataset.
 #' @export
-abcdepth <- function(data, point) {
+abcdepth <- function(data, point, quiet=TRUE) {
   ABCDepth <- rJava::.jnew("org.prng.ea.tukey.ABCDepth")
 
   jmat <- jdx::convertToJava(as.matrix(data))
@@ -13,7 +14,7 @@ abcdepth <- function(data, point) {
   jpt <- jdx::convertToJava(as.numeric(point))
   jpt <- rJava::.jcast(jpt, "[D")
 
-  depth <- rJava::.jcall(ABCDepth, returnSig = "D", "calculateDepth", jmat, jpt)
+  depth <- rJava::.jcall(ABCDepth, returnSig = "D", "calculateDepth", jmat, jpt, quiet)
 
   return(depth)
 }
